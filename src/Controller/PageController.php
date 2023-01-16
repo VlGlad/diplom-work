5987294301565926e5b86165f0a087a82cdb0c61
VlGlad <kva-2001@yandex.ru>
2023-01-16 17:52:35 +0500

    Controllers separated, user page added

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -4,10 +4,7 @@ namespace App\Controller;
 
 use App\Entity\MediaFile;
 use App\Entity\User;
-use App\Form\MediaFileType;
-use App\Service\FileUploader;
 use Doctrine\Persistence\ManagerRegistry;
-use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -29,42 +26,6 @@ class PageController extends AbstractController
         dump($user);
         return $this->render('page/index.html.twig', [
             'images' => $mediaFiles,
-        ]);
-    }
-
-    /* #[Route('/app/search', name: 'app_searchUser')]
-    public function searchUser()
-    {
-        
-    } */
-
-    #[Route('/app/add', name: 'app_addpage')]
-    public function index(#[CurrentUser] ?User $user, Request $request, FileUploader $fileUploader, ManagerRegistry $doctrine, LoggerInterface $logger): Response
-    {
-        $media = new MediaFile();
-        $form = $this->createForm(MediaFileType::class, $media);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()){
-            $media = $form->getData();
-            $mediaFile = $form->get('file_url')->getData();
-            if ($mediaFile){
-                $mediaFileName = $fileUploader->upload($mediaFile);
-                $media
-                    ->setFileUrl($mediaFileName)
-                    ->setType(1)
-                    ->setFileLikes(0)
-                    ->setFileOwner($user->getId())
-                ;
-                $entityManager = $doctrine->getManager();
-                $entityManager->persist($media);
-                $entityManager->flush();
-                return $this->redirectToRoute('app_homepage');
-            }
-        }
-        return $this->render('page/add.html.twig', [
-            "title" => "hello",
-            'addForm' => $form,
         ]);
     }
 }
