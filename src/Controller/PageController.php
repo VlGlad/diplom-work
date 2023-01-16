de3698ea5497b42f0885b64c80f21c80acb3af57
VlGlad <kva-2001@yandex.ru>
2023-01-16 11:42:19 +0500

    Linking pages together

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\MediaFile;
+use App\Entity\User;
 use App\Form\MediaFileType;
 use App\Service\FileUploader;
 use Doctrine\Persistence\ManagerRegistry;
@@ -11,34 +12,50 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class PageController extends AbstractController
 {
-    #[Route('/', name: 'app_homepage')]
-    public function browse(ManagerRegistry $doctrine): Response
+    #[Route('/', name: 'app_frontpage')]
+    public function frontpage()
     {
-        $mediaFiles = $doctrine->getRepository(MediaFile::class)->findAll();
-        dump($mediaFiles);
+        return $this->render("page/frontpage.html.twig");
+    }
+
+    #[Route('/app', name: 'app_homepage')]
+    public function browse(#[CurrentUser] ?User $user,  ManagerRegistry $doctrine): Response
+    {
+        $mediaFiles = $doctrine->getRepository(MediaFile::class)->findBy(['file_owner' => $user->getId()]);
+        dump($user);
         return $this->render('page/index.html.twig', [
             'images' => $mediaFiles,
         ]);
     }
 
-    #[Route('/add', name: 'app_addpage')]
-    public function index(Request $request, FileUploader $fileUploader, ManagerRegistry $doctrine, LoggerInterface $logger): Response
+    /* #[Route('/app/search', name: 'app_searchUser')]
+    public function searchUser()
+    {
+        
+    } */
+
+    #[Route('/app/add', name: 'app_addpage')]
+    public function index(#[CurrentUser] ?User $user, Request $request, FileUploader $fileUploader, ManagerRegistry $doctrine, LoggerInterface $logger): Response
     {
         $media = new MediaFile();
         $form = $this->createForm(MediaFileType::class, $media);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()){
-            $logger->info('FORM IS SUBMITTED');
             $media = $form->getData();
             $mediaFile = $form->get('file_url')->getData();
             if ($mediaFile){
-                $logger->info('FILE NAME IS FINE');
                 $mediaFileName = $fileUploader->upload($mediaFile);
-                $media->setFileUrl($mediaFileName);
+                $media
+                    ->setFileUrl($mediaFileName)
+                    ->setType(1)
+                    ->setFileLikes(0)
+                    ->setFileOwner($user->getId())
+                ;
                 $entityManager = $doctrine->getManager();
                 $entityManager->persist($media);
                 $entityManager->flush();
@@ -47,7 +64,7 @@ class PageController extends AbstractController
         }
         return $this->render('page/add.html.twig', [
             "title" => "hello",
-            'form' => $form,
+            'addForm' => $form,
         ]);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,6 +10,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['nickname'], message: 'There is already an account with this nickname')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -28,6 +29,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     #[ORM\Column]
     private ?string $password = null;
+
+    #[ORM\Column(length: 180)]
+    private ?string $nickname = null;
 
     public function getId(): ?int
     {
@@ -97,5 +101,17 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getNickname(): ?string
+    {
+        return $this->nickname;
+    }
+
+    public function setNickname(string $nickname): self
+    {
+        $this->nickname = $nickname;
+
+        return $this;
     }
 }
